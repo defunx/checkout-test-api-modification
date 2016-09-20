3cce1e3729c433170ba491f966e7ce9d1b6eda5a
defunx <defunx>
2016-09-20 22:13:28 +0100

    Push project to the repo

---
+++ Checkout.ApiClient.Net45/ApiServices/ShoppingList/ResponseModels/ShoppingList.cs
@@ -0,0 +1,14 @@
+﻿using Checkout.ApiServices.ShoppingList.RequestModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout.ApiServices.ShoppingList.ResponseModels
+{
+    public class ShoppingList
+    {
+        public List<ShoppingListItem> Data { get; set; }
+    }
+}
---
+++ Checkout.ApiClient.Net45/ApiServices/ShoppingList/ShoppingListService.cs
@@ -0,0 +1,56 @@
+﻿using Checkout.ApiServices.SharedModels;
+using Checkout.ApiServices.ShoppingList.RequestModels;
+using Checkout.ApiServices.ShoppingList.ResponseModels;
+using Checkout.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout.ApiServices.ShoppingList
+{
+    public class ShoppingListService
+    {
+
+        public HttpResponse<ShoppingListItem> AddShoppingListItem(ShoppingListItem requestModel)
+        {
+            return new ApiHttpClient().PostRequest<ShoppingListItem>(ApiUrls.ShoppingList, AppSettings.SecretKey, requestModel);
+        }
+
+        public HttpResponse<OkResponse> UpdateShoppingListItem(ShoppingListItem requestModel)
+        {
+            var updateShoppingListUri = ApiUrls.ShoppingList;
+            return new ApiHttpClient().PutRequest<OkResponse>(updateShoppingListUri, AppSettings.SecretKey, requestModel);
+        }
+
+        public HttpResponse<OkResponse> DeleteShoppingListItem(string itemName)
+        {
+            var deleteShoppingListItemUri = string.Format(ApiUrls.ShoppingListItemDelete, itemName);
+            return new ApiHttpClient().DeleteRequest<OkResponse>(deleteShoppingListItemUri, AppSettings.SecretKey);
+        }
+
+        public HttpResponse<ShoppingListItem> GetShoppingListItem(string itemName)
+        {
+            var getShoppingListItemUri = string.Format(ApiUrls.ShoppingListItem, itemName);
+            return new ApiHttpClient().GetRequest<ShoppingListItem>(getShoppingListItemUri, AppSettings.SecretKey);
+        }
+
+        public HttpResponse<Checkout.ApiServices.ShoppingList.ResponseModels.ShoppingList> GetShoppingList(ShoppingItemGetList request)
+        {
+            var getShoppingListUri = ApiUrls.ShoppingList;
+
+            if (request.PageSize.HasValue)
+            {
+                getShoppingListUri = UrlHelper.AddParameterToUrl(getShoppingListUri, "pageSize", request.PageSize.ToString());
+            }
+
+            if (!string.IsNullOrEmpty(request.PageNumber))
+            {
+                getShoppingListUri = UrlHelper.AddParameterToUrl(getShoppingListUri, "pageNumber", request.PageNumber);
+            }
+
+            return new ApiHttpClient().GetRequest<Checkout.ApiServices.ShoppingList.ResponseModels.ShoppingList>(getShoppingListUri, AppSettings.SecretKey);
+        }
+    }
+}
